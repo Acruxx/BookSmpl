f7a29c0a407a72b5819bcfd34276cdd720732a23
acruxx <chsnot@gmail.com>
2013-07-21 17:32:02 +0200

    added model for account tabel

--- book/models.py
+++ book/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+	account_number = models.IntegerField(
+		primary_key = True,
+		)
+	account_name = models.CharField(
+		max_length = 255,
+		)
+	account_description = models.TextField(
+		max_length = 400,
+		)
+
+	def __str__(self):
+		
+		return ' '.join([
+			self.account_number,
+			self.account_name,
+		])
