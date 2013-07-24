565aad5f91135be88ac55fe688eecf6a533a68f7
acruxx <chsnot@gmail.com>
2013-07-24 20:46:04 +0200

    general progress + model for verifications db

--- book/models.py
+++ book/models.py
@@ -15,3 +15,29 @@ class Account(models.Model):
 	def __str__(self):
 		
 		return self.account_name
+
+class Verification(models.Model):
+	verification_number = models.AutoField(
+		primary_key = True,
+		)
+	verification_date = models.DateField()
+	expiration_date = models.DateField()
+	verification_type = models.CharField(
+		max_length = 25
+		)
+	verification_desc = models.CharField(
+		max_length = 50
+		)
+	creation_date = models.DateField(
+		auto_now_add = True
+		)
+	last_edit_date = models.DateField(
+		auto_now = True
+		)
+	class Meta:
+		verbose_name = ('Verification')
+    	verbose_name_plural = ('Verifications')
+
+	def __unicode__(self):
+		pass
+    
